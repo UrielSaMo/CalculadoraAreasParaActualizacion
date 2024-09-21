1f1f807a101f9530ebc636eeaf6a42f6fe8b3b9e
uriel <uriel@MSI>
2024-09-20 18:18:35 -0600

    Agregar archivos de proyecto.

---
+++ CalculadoraAreasParaActualizacion/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace CalculadoraAreasParaActualizacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonRombo = new System.Windows.Forms.Button();
+            this.buttonRomboide = new System.Windows.Forms.Button();
+            this.panelRombo = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxResultadoRombo = new System.Windows.Forms.TextBox();
+            this.buttonCalcularRombo = new System.Windows.Forms.Button();
+            this.textBoxDiametroM = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelRomboide = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxResultadoRomboide = new System.Windows.Forms.TextBox();
+            this.buttonCalcularRomboide = new System.Windows.Forms.Button();
+            this.textBoxAlturaR = new System.Windows.Forms.TextBox();
+            this.textBoxBaseR = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDiametroMenor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panelRombo.SuspendLayout();
+            this.panelRomboide.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.buttonRombo);
+            this.flowLayoutPanel1.Controls.Add(this.buttonRomboide);
+            this.flowLayoutPanel1.Controls.Add(this.panelRombo);
+            this.flowLayoutPanel1.Controls.Add(this.panelRomboide);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(361, 348);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // buttonRombo
+            // 
+            this.buttonRombo.Location = new System.Drawing.Point(3, 3);
+            this.buttonRombo.Name = "buttonRombo";
+            this.buttonRombo.Size = new System.Drawing.Size(75, 23);
+            this.buttonRombo.TabIndex = 2;
+            this.buttonRombo.Text = "Rombo";
+            this.buttonRombo.UseVisualStyleBackColor = true;
+            this.buttonRombo.Click += new System.EventHandler(this.buttonRombo_Click);
+            // 
+            // buttonRomboide
+            // 
+            this.buttonRomboide.Location = new System.Drawing.Point(84, 3);
+            this.buttonRomboide.Name = "buttonRomboide";
+            this.buttonRomboide.Size = new System.Drawing.Size(75, 23);
+            this.buttonRomboide.TabIndex = 1;
+            this.buttonRomboide.Text = "Romboide";
+            this.buttonRomboide.UseVisualStyleBackColor = true;
+            this.buttonRomboide.Click += new System.EventHandler(this.buttonRomboide_Click);
+            // 
+            // panelRombo
+            // 
+            this.panelRombo.Controls.Add(this.textBoxDiametroMenor);
+            this.panelRombo.Controls.Add(this.label1);
+            this.panelRombo.Controls.Add(this.label7);
+            this.panelRombo.Controls.Add(this.label5);
+            this.panelRombo.Controls.Add(this.textBoxResultadoRombo);
+            this.panelRombo.Controls.Add(this.buttonCalcularRombo);
+            this.panelRombo.Controls.Add(this.textBoxDiametroM);
+            this.panelRombo.Controls.Add(this.label2);
+            this.panelRombo.Location = new System.Drawing.Point(3, 32);
+            this.panelRombo.Name = "panelRombo";
+            this.panelRombo.Size = new System.Drawing.Size(340, 152);
+            this.panelRombo.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(129, 10);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Rombo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(168, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Resultado";
+            // 
+            // textBoxResultadoRombo
+            // 
+            this.textBoxResultadoRombo.Location = new System.Drawing.Point(168, 100);
+            this.textBoxResultadoRombo.Name = "textBoxResultadoRombo";
+            this.textBoxResultadoRombo.ReadOnly = true;
+            this.textBoxResultadoRombo.Size = new System.Drawing.Size(132, 20);
+            this.textBoxResultadoRombo.TabIndex = 3;
+            // 
+            // buttonCalcularRombo
+            // 
+            this.buttonCalcularRombo.Location = new System.Drawing.Point(168, 49);
+            this.buttonCalcularRombo.Name = "buttonCalcularRombo";
+            this.buttonCalcularRombo.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalcularRombo.TabIndex = 2;
+            this.buttonCalcularRombo.Text = "Calcular";
+            this.buttonCalcularRombo.UseVisualStyleBackColor = true;
+            this.buttonCalcularRombo.Click += new System.EventHandler(this.buttonCalcularRombo_Click);
+            // 
+            // textBoxDiametroM
+            // 
+            this.textBoxDiametroM.Location = new System.Drawing.Point(18, 52);
+            this.textBoxDiametroM.Name = "textBoxDiametroM";
+            this.textBoxDiametroM.Size = new System.Drawing.Size(132, 20);
+            this.textBoxDiametroM.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ingresa el diametro mayor";
+            // 
+            // panelRomboide
+            // 
+            this.panelRomboide.Controls.Add(this.label8);
+            this.panelRomboide.Controls.Add(this.label6);
+            this.panelRomboide.Controls.Add(this.textBoxResultadoRomboide);
+            this.panelRomboide.Controls.Add(this.buttonCalcularRomboide);
+            this.panelRomboide.Controls.Add(this.textBoxAlturaR);
+            this.panelRomboide.Controls.Add(this.textBoxBaseR);
+            this.panelRomboide.Controls.Add(this.label4);
+            this.panelRomboide.Controls.Add(this.label3);
+            this.panelRomboide.Location = new System.Drawing.Point(3, 190);
+            this.panelRomboide.Name = "panelRomboide";
+            this.panelRomboide.Size = new System.Drawing.Size(340, 147);
+            this.panelRomboide.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(129, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Romboide";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(168, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Resultado";
+            // 
+            // textBoxResultadoRomboide
+            // 
+            this.textBoxResultadoRomboide.Location = new System.Drawing.Point(168, 104);
+            this.textBoxResultadoRomboide.Name = "textBoxResultadoRomboide";
+            this.textBoxResultadoRomboide.ReadOnly = true;
+            this.textBoxResultadoRomboide.Size = new System.Drawing.Size(132, 20);
+            this.textBoxResultadoRomboide.TabIndex = 4;
+            // 
+            // buttonCalcularRomboide
+            // 
+            this.buttonCalcularRomboide.Location = new System.Drawing.Point(168, 49);
+            this.buttonCalcularRomboide.Name = "buttonCalcularRomboide";
+            this.buttonCalcularRomboide.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalcularRomboide.TabIndex = 3;
+            this.buttonCalcularRomboide.Text = "Calcular";
+            this.buttonCalcularRomboide.UseVisualStyleBackColor = true;
+            this.buttonCalcularRomboide.Click += new System.EventHandler(this.buttonCalcularRomboide_Click);
+            // 
+            // textBoxAlturaR
+            // 
+            this.textBoxAlturaR.Location = new System.Drawing.Point(19, 104);
+            this.textBoxAlturaR.Name = "textBoxAlturaR";
+            this.textBoxAlturaR.Size = new System.Drawing.Size(132, 20);
+            this.textBoxAlturaR.TabIndex = 3;
+            // 
+            // textBoxBaseR
+            // 
+            this.textBoxBaseR.Location = new System.Drawing.Point(19, 52);
+            this.textBoxBaseR.Name = "textBoxBaseR";
+            this.textBoxBaseR.Size = new System.Drawing.Size(129, 20);
+            this.textBoxBaseR.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ingrese la altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ingrese la base";
+            // 
+            // textBoxDiametroMenor
+            // 
+            this.textBoxDiametroMenor.Location = new System.Drawing.Point(18, 103);
+            this.textBoxDiametroMenor.Name = "textBoxDiametroMenor";
+            this.textBoxDiametroMenor.Size = new System.Drawing.Size(132, 20);
+            this.textBoxDiametroMenor.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ingresa el diametro menor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 207);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panelRombo.ResumeLayout(false);
+            this.panelRombo.PerformLayout();
+            this.panelRomboide.ResumeLayout(false);
+            this.panelRomboide.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonRombo;
+        private System.Windows.Forms.Button buttonRomboide;
+        private System.Windows.Forms.Panel panelRombo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxResultadoRombo;
+        private System.Windows.Forms.Button buttonCalcularRombo;
+        private System.Windows.Forms.TextBox textBoxDiametroM;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panelRomboide;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxResultadoRomboide;
+        private System.Windows.Forms.Button buttonCalcularRomboide;
+        private System.Windows.Forms.TextBox textBoxAlturaR;
+        private System.Windows.Forms.TextBox textBoxBaseR;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDiametroMenor;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CalculadoraAreasParaActualizacion/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraAreasParaActualizacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panelRombo.Visible = false;
+            panelRomboide.Visible = false;
+        }
+
+        private void buttonRombo_Click(object sender, EventArgs e)
+        {
+            MostrarPanel(panelRombo);
+        }
+
+        private void buttonRomboide_Click(object sender, EventArgs e)
+        {
+            MostrarPanel(panelRomboide);
+        }
+        private void MostrarPanel(Panel panelSeleccionado)
+        {
+            // Oculta todos los paneles
+            panelRomboide.Visible = false;
+            panelRombo.Visible = false;
+
+            // Muestra el panel seleccionado
+            panelSeleccionado.Visible = true;
+        }
+
+        private void buttonCalcularRombo_Click(object sender, EventArgs e)
+        {
+            double baseT, altura, res;
+
+            string texto = textBoxDiametroM.Text;
+            string texto2 = textBoxDiametroMenor.Text;
+
+            bool NumeroValido = double.TryParse(textBoxDiametroM.Text, out baseT);
+            bool NumeroValido2 = double.TryParse(textBoxDiametroMenor.Text, out altura);
+
+            if (Regex.IsMatch(texto, @"^\d+(\.\d+)?$") && Regex.IsMatch(texto2, @"^\d+(\.\d+)?$"))
+            {
+                if (NumeroValido && NumeroValido2)
+                {
+                    baseT = double.Parse(textBoxDiametroM.Text);
+                    altura = double.Parse(textBoxDiametroMenor.Text);
+                    res = (baseT * altura) / 2;
+                    textBoxResultadoRombo.Text = res.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingresa un número válido.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un número válido (sin caracteres especiales o letras).");
+            }
+        }
+
+        private void buttonCalcularRomboide_Click(object sender, EventArgs e)
+        {
+            double baseT, altura, res;
+
+            string texto = textBoxBaseR.Text;
+            string texto2 = textBoxAlturaR.Text;
+
+            bool NumeroValido = double.TryParse(textBoxBaseR.Text, out baseT);
+            bool NumeroValido2 = double.TryParse(textBoxAlturaR.Text, out altura);
+
+            if (Regex.IsMatch(texto, @"^\d+(\.\d+)?$") && Regex.IsMatch(texto2, @"^\d+(\.\d+)?$"))
+            {
+                if (NumeroValido && NumeroValido2)
+                {
+                    baseT = double.Parse(textBoxBaseR.Text);
+                    altura = double.Parse(textBoxAlturaR.Text);
+                    res = baseT * altura;
+                    textBoxResultadoRomboide.Text = res.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingresa un número válido.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un número válido (sin caracteres especiales o letras).");
+            }
+        }
+    }
+}
